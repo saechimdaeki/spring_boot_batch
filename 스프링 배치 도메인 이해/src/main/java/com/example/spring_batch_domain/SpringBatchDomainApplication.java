21c77833685b23b860dd00cee30aa7956cdea75b
saechim <anima94@kakao.com>
2021-11-14 19:34:44 +0900

    스프링 배치 도메인 이해 :  Job :jack_o_lantern:

---
+++ 스프링 배치 도메인 이해/src/main/java/com/example/spring_batch_domain/SpringBatchDomainApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_batch_domain;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchDomainApplication.class, args);
+    }
+
+}
