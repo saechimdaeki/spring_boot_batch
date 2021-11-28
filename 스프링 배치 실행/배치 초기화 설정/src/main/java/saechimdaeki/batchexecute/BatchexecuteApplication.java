f3efc2601b568f632348a5dea5038a723b22fd93
saechim <anima94@kakao.com>
2021-11-28 14:23:19 +0900

    스프링 배치 실행: 배치 초기화 설정 :black_joker:

---
+++ 스프링 배치 실행/배치 초기화 설정/src/main/java/saechimdaeki/batchexecute/BatchexecuteApplication.java
@@ -0,0 +1,15 @@
+package saechimdaeki.batchexecute;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchexecuteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchexecuteApplication.class, args);
+    }
+
+}
