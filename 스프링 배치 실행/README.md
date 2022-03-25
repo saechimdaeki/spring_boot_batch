d6811f5ea38cf24e8a69c0be433b2ca034bbbfed
saechimdaeki <anima94@kakao.com>
2022-03-25 22:45:39 +0900

    Transition - on() / to() / stop(), fail(), end(), stopAndRestart()

--- 스프링 배치 실행/README.md
+++ 스프링 배치 실행/README.md
@@ -369,3 +369,61 @@ public Job batchJob(){
 
 ![image](https://user-images.githubusercontent.com/40031858/160075565-efb283aa-f683-4708-91bd-ddb89af96492.png)
 
+## Transition -  on()/ to()/ stop(),fail(),end(),stopAndRestart()
+```java
+public Job batchJob(){
+  return jobBuilderFactory.get("batchJob")
+        .start(Flow)
+        .next(Step or Flow or JobExecutionDecider)
+        .on(String pattern) //TransitionBuilder 반환
+        .to(Step or Flow or JobExecutionDecider)
+        .stop()/fail()/end()/stopAndRestart(Step or Flow or JobExecutionDecider)
+        .end()
+        .build();
+}
+
+```
+- 기본 개념
+  - `Transition`
+    - Flow 내 Step의 조건부 전환(전이)을 정의함
+    - Job의 API 설정에서 on(String pattern) 메소드를 호출하면 TransitionBuilder가 반환되어 Transition Flow를 구성할 수 있음
+    - Step의 종료상태(ExitStatus) 가 어떤 pattern과도 매칭되지 않으면 스프링 배치에서 예외를 발생하고 Job은 실패
+    - transition은 구체적인 것부터 그렇지 않은 순서로 적용된다
+  - `API`
+    - `on(String pattern)`
+      - Step의 실행 결과로 돌려받는 종료상태(ExitStatus)와 매칭하는 패턴 스키마, BatchStatus와 매칭하는 것이 아님
+      - pattern과 ExitStatus와 매칭이 되면 다음으로 실행할 Step을 지정할 수 있다
+      - 특수문자는 두 가지만 허용
+        - "*": 0 개이상의 문자와 매칭, 모든 ExitStatus와 매칭된다
+        - "?": 정확히 1개의 문자와 매칭
+        - ex) "c*t"는 "cat"과 "count"에 매칭되고 ,"c?t"는 "cat"에는 매칭되지만 "count"는 X
+  - `to()`
+    - 다음으로 실행할 단계를 지정
+  - `from()`
+    - 이전 단계에서 정의한 Transition을 새롭게 추가 정의함
+
+---
+- `Job을 중단하거나 종료하는 Transition API`
+  - Flow가 실행되면 FlowExecutionStatus에 상태값이 저장되고 최종적으로 Job의 BatchStatus와 ExitStatus에 반영된다
+  - Step의 BatchStatus 및 ExitStatus에는 아무런 영향을 주지 않고 Job의 상태만을 변경한다
+  - `stop()`
+    - FlowExecutionStatus가 STOPPED 상태로 종료되는 transition
+    - Job의 BatchStatus와 ExitStatus가 STOPPED으로 종료됨
+  - `fail()`
+    - FlowExecutionStatus가 FAILED 상태로 종료되는 transition
+    - Job의 BatchStatus와 ExitStatus가 FAILED으로 종료됨
+  - `end()`
+    - FlowExecutionStatus가 COMPLETED 상태로 종료되는 transition
+    - Job의 BatchStatus와 ExitStatus가 COMPLETED으로 종료됨
+    - Step의 ExitStatus가 FAILED이더라도 Job의 BatchStatus가 COMPLETED로 종료하도록 가능하며 이 때 Job의 재시작은 불가능
+  - `stopAndRestart(Step or Flow or JobExecutionDecider)`
+    - stop() transition과 기본 흐름은 도일
+    - 특정 step에서 작업을 중단하도록 설정하면 중단 이전의 Step만 COMPLETED 저장되고 이후의 step은 실행되지 않고 STOPPED 상태로 Job종료
+    - Job이 다시 실행됐을 때 실행해야 할 step을 restart인자로 넘기면 이전에 COMPLETED로 저장된 step은 건너뛰고 중단 이후 step부터 시작 
+
+
+![image](https://user-images.githubusercontent.com/40031858/160128198-ec3cdc1c-62ea-418b-80de-e7faf949dd91.png)
+
+![image](https://user-images.githubusercontent.com/40031858/160128271-497be716-00d9-4158-b2fa-5cc9b532dc68.png)
+
+![image](https://user-images.githubusercontent.com/40031858/160128318-6a7a8049-ba12-46ae-92e6-f7889016719a.png)
--- 스프링 배치 실행/spring_batch_execute-flow/src/main/java/com/example/springbatch/BatchStatusExitStatusConfiguration.java
+++ 스프링 배치 실행/spring_batch_execute-flow/src/main/java/com/example/springbatch/BatchStatusExitStatusConfiguration.java
@@ -19,53 +19,53 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class BatchStatusExitStatusConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+////    @Bean
+////    public Job batchJob(){
+////        return jobBuilderFactory.get("batchJob")
+////                .start(step1())
+////                .next(step2())
+////                .build();
+////    }
+//
 //    @Bean
 //    public Job batchJob(){
 //        return jobBuilderFactory.get("batchJob")
 //                .start(step1())
-//                .next(step2())
+//                .on("FAILED")
+//                .to(step2())
+//                .end()
 //                .build();
 //    }
-
-    @Bean
-    public Job batchJob(){
-        return jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .on("FAILED")
-                .to(step2())
-                .end()
-                .build();
-    }
-
-
-
-
-    @Bean
-    public Step step1(){
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step1 has executed");
-                        stepContribution.setExitStatus(ExitStatus.FAILED);
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-
-    @Bean
-    public Step step2(){
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step2 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
+//
+//
+//
+//
+//    @Bean
+//    public Step step1(){
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println(">> step1 has executed");
+//                        stepContribution.setExitStatus(ExitStatus.FAILED);
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+//    }
+//
+//    @Bean
+//    public Step step2(){
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println(">> step2 has executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+//    }
 
 }
