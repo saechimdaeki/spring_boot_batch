9ef6542b25ab67acc0105f7c4e95348d7561475d
saechimdaeki <anima94@kakao.com>
2022-03-31 18:45:11 +0900

    FaultTolerant

--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 - ## [스프링 배치 청크 프로세스 활용 - ItemWriter](https://github.com/saechimdaeki/spring_boot_batch/tree/main/%EC%8A%A4%ED%94%84%EB%A7%81%20%EB%B0%B0%EC%B9%98%20%EC%B2%AD%ED%81%AC%20%ED%94%84%EB%A1%9C%EC%84%B8%EC%8A%A4%20%ED%99%9C%EC%9A%A9%20-%20ItemWriter)
 
 - ## [스프링 배치 청크 프로세스 활용 - ItemProcessor](https://github.com/saechimdaeki/spring_boot_batch/tree/main/%EC%8A%A4%ED%94%84%EB%A7%81%20%EB%B0%B0%EC%B9%98%20%EC%B2%AD%ED%81%AC%20%ED%94%84%EB%A1%9C%EC%84%B8%EC%8A%A4%20%ED%99%9C%EC%9A%A9%20-%20ItemProcessor)
+
+- ## [스프링 배치 반복 및 오류 제어](https://github.com/saechimdaeki/spring_boot_batch/tree/main/%EC%8A%A4%ED%94%84%EB%A7%81%20%EB%B0%B0%EC%B9%98%20%EB%B0%98%EB%B3%B5%20%EB%B0%8F%20%EC%98%A4%EB%A5%98%20%EC%A0%9C%EC%96%B4)
+
--- 스프링 배치 반복 및 오류 제어/spring_batch_iterative_and_error_control/src/main/java/com/example/springbatch/RepeatConfiguration.java
+++ 스프링 배치 반복 및 오류 제어/spring_batch_iterative_and_error_control/src/main/java/com/example/springbatch/RepeatConfiguration.java
@@ -23,72 +23,72 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 public class RepeatConfiguration {
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job batchJob(){
-        return jobBuilderFactory.get("batchJob")
-                .incrementer(new RunIdIncrementer())
-                .start(step1())
-                .build();
-    }
-
-    @Bean
-    public Step step1(){
-        return stepBuilderFactory.get("step1")
-                .<String,String>chunk(5)
-                .reader(new ItemReader<String>() {
-
-                    int i=0;
-                    @Override
-                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-                        i++;
-                        return i>3 ? null : "item" +i;
-                    }
-                })
-                .processor(new ItemProcessor<String,String>(){
-
-                    RepeatTemplate repeatTemplate=new RepeatTemplate();
-
-
-                    @Override
-                    public String process(String s) throws Exception {
-
-//                        repeatTemplate.setCompletionPolicy(new SimpleCompletionPolicy(3));
-//                        repeatTemplate.setCompletionPolicy(new TimeoutTerminationPolicy(3000));
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
 //
-//                        CompositeCompletionPolicy compositeCompletionPolicy=new CompositeCompletionPolicy();
-//                        CompletionPolicy[] completionPolicies = new CompletionPolicy[]{
-//                                new SimpleCompletionPolicy(3),
-//                                new TimeoutTerminationPolicy(3000)};
+//    @Bean
+//    public Job batchJob(){
+//        return jobBuilderFactory.get("batchJob")
+//                .incrementer(new RunIdIncrementer())
+//                .start(step1())
+//                .build();
+//    }
 //
-//                        compositeCompletionPolicy.setPolicies(completionPolicies);
-//                        repeatTemplate.setCompletionPolicy(compositeCompletionPolicy);
-
-                        repeatTemplate.setExceptionHandler(simpleLimitExceptionHandler());
-
-
-                        repeatTemplate.iterate(new RepeatCallback() {
-                            @Override
-                            public RepeatStatus doInIteration(RepeatContext repeatContext) throws Exception {
-
-                                System.out.println("repeatTemplate is testing");
-                                throw new RuntimeException("Exception is occurred");
-//                                return RepeatStatus.CONTINUABLE;
-                            }
-                        });
-
-                        return s;
-
-                    }
-                })
-                .writer(items -> System.out.println(items))
-                .build();
-    }
-
-    @Bean
-    public ExceptionHandler simpleLimitExceptionHandler(){
-        return new SimpleLimitExceptionHandler(3);
-    }
+//    @Bean
+//    public Step step1(){
+//        return stepBuilderFactory.get("step1")
+//                .<String,String>chunk(5)
+//                .reader(new ItemReader<String>() {
+//
+//                    int i=0;
+//                    @Override
+//                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+//                        i++;
+//                        return i>3 ? null : "item" +i;
+//                    }
+//                })
+//                .processor(new ItemProcessor<String,String>(){
+//
+//                    RepeatTemplate repeatTemplate=new RepeatTemplate();
+//
+//
+//                    @Override
+//                    public String process(String s) throws Exception {
+//
+////                        repeatTemplate.setCompletionPolicy(new SimpleCompletionPolicy(3));
+////                        repeatTemplate.setCompletionPolicy(new TimeoutTerminationPolicy(3000));
+////
+////                        CompositeCompletionPolicy compositeCompletionPolicy=new CompositeCompletionPolicy();
+////                        CompletionPolicy[] completionPolicies = new CompletionPolicy[]{
+////                                new SimpleCompletionPolicy(3),
+////                                new TimeoutTerminationPolicy(3000)};
+////
+////                        compositeCompletionPolicy.setPolicies(completionPolicies);
+////                        repeatTemplate.setCompletionPolicy(compositeCompletionPolicy);
+//
+//                        repeatTemplate.setExceptionHandler(simpleLimitExceptionHandler());
+//
+//
+//                        repeatTemplate.iterate(new RepeatCallback() {
+//                            @Override
+//                            public RepeatStatus doInIteration(RepeatContext repeatContext) throws Exception {
+//
+//                                System.out.println("repeatTemplate is testing");
+//                                throw new RuntimeException("Exception is occurred");
+////                                return RepeatStatus.CONTINUABLE;
+//                            }
+//                        });
+//
+//                        return s;
+//
+//                    }
+//                })
+//                .writer(items -> System.out.println(items))
+//                .build();
+//    }
+//
+//    @Bean
+//    public ExceptionHandler simpleLimitExceptionHandler(){
+//        return new SimpleLimitExceptionHandler(3);
+//    }
 }
